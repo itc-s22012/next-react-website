a64c04a722481634f9f908a61d25eb78db1509aa
itc-s22012 <s22012@std.it-college.ac.jp>
2023-02-08 07:39:54 +0000

    Chapter 9.6

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -101,7 +101,7 @@ export async function getStaticProps (context) {
       categories: post.categories,
       description: description,
       prevPost: prevPost,
-      nextPost: nextPost,
+      nextPost: nextPost
     }
   }
 }
